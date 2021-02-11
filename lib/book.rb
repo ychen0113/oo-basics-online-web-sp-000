3a9fdf9c5b73c8d8e741d1784149d0f96402675f
Yiliang Chen <yiliangchen0113@gmail.com>
2021-02-11 19:22:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,16 @@
 class Book
-  def initialize(title)
+  def initialize(title, author)
     @title = title
+    @author = author
   end 
   
   def title 
     @title 
   end 
+  
+  def author 
+    @author
+  end
   
 end 
 
