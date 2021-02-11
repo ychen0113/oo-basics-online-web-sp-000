cfc776e8ad6c1cbcef95bc6333f04e9cfd9c286f
Yiliang Chen <yiliangchen0113@gmail.com>
2021-02-11 19:32:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,17 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end 
+  def title 
+    @title 
+  end 
   
-attr_accessor :title 
-attr_accessor :author
-attr_accessor :page_count
-attr_accessor :genre 
-
-def turn_page 
-  puts "Flipping the page...wow, you read fast!"
+  def turn_page 
+   puts "Flipping the page...wow, you read fast!"
   
-end 
+  end 
 
 end 
 
