726650a28346c489db4771a3f8a27c44f9d624c4
Yiliang Chen <yiliangchen0113@gmail.com>
2021-02-11 19:41:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,13 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :color, :size, :material, :condition, :cobble 
-  
-  
-  
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand 
   def initialize(brand)
     @brand = brand 
   end 
   
-  
+  def cobble 
+    
+  end 
   
 end 
