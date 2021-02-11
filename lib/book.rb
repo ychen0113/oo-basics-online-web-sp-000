8d88c8fe3e33fa5954a6ee18e60f3ea9704e71e9
Yiliang Chen <yiliangchen0113@gmail.com>
2021-02-11 19:28:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,5 +7,11 @@ attr_accessor :title
 attr_accessor :author
 attr_accessor :page_count
 attr_accessor :genre 
+
+def turn_page 
+  puts "Flipping the page ... wow, you read fast!"
+  
+end 
+
 end 
 
